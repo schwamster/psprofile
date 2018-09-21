b50f8a86f6b59c7e8bea60d0327efe73d9ee3b0e
schwamster <bastian.toepfer@gmail.com>
2018-09-21 23:19:47 +0200

    init

---
+++ Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,34 @@
+Add-Type -Assembly System.Web
+function google{
+    param([string] $query)
+    $query = "https://www.google.com/#q=" + [System.Web.HttpUtility]::UrlEncode($query)
+    Start-Process $query
+}
+
+function open{
+    param([string] $url)
+    
+    if($url -eq "gmail"){
+        $url = "https://gmail.com"
+    }
+
+    if($url -eq "spiegel"){
+        $url = "https://www.spiegel.de"
+    }
+
+    if($url -eq "github"){
+        $url = "https://github.com"
+    }
+
+    if($url -eq "dev"){
+        $url = "https://dev.to"
+    }
+
+    if(! $url.StartsWith("http")){
+        $url = "https://" + $url
+    }
+
+    
+
+    Start-Process $url
+}
